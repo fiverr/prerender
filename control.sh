627922875778156549cdcc44c26b6ce390c5c910
RoeiA <roei.a@fiverr.com>
2015-08-04 18:11:35 +0300

    [CONFIG] Fix control.sh

--- control.sh
+++ control.sh
@@ -5,7 +5,7 @@ start ()
 	export PHANTOM_CLUSTER_NUM_WORKERS=4
 	export PORT=6018
 	export CACHE_TTL=14400 # In seconds = 4 hours
-	if [ '$environment' == 'production' ]
+	if [ $environment == 'production' ]
 	then
 		export MEMCACHED_SERVERS=memcached05:5555,memcached06:5555,memcached07:5555,memcached08:5555
 	else
